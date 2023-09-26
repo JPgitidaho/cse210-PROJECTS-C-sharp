e3cb5aa0e108a16ffb6ee11b0a7e06587f53ad00
JPgitidaho <per22030@byui.edu>
2023-09-26 10:40:00 -0300

    edit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,19 +3,24 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        //Initialize a random number generator
         Random random = new Random();
         string playAgain = "yes";
 
+        //Star a Loop to allow the user to play multiple times
         while (playAgain.ToLower() == "yes")
         {
+            //generate a random magic number between 1 and 200
             int magicNumber = random.Next(1, 201);
             int numberOfGuesses = 0;
             int userGuess;
 
+            //display a welcome message
             Console.WriteLine("Welcome to Guess My Number!");
             Console.WriteLine("I'm thinking of a number between 1 and 200.");
 
+            //star a loop for game
             do
             {
                 Console.Write("What is your guess? ");
@@ -44,9 +49,13 @@ class Program
                     Console.WriteLine("Pease enter a valid number.");
                 }
             } while (userGuess != magicNumber);
+
+            //ask if user wants to play again
             Console.Write("Do you want to play again (yes/no): ");
             playAgain = Console.ReadLine();
         }
+
+        //thank the user for playing when they are done
         Console.WriteLine("Thank you for playing!");
     }
 }
