fa0e15ba9bc7001a96da6414ff8774862d429056
JPgitidaho <122384707+JPgitidaho@users.noreply.github.com>
2023-10-19 18:36:55 -0300

    edit project

--- prove/Develop03/Programs.cs
+++ prove/Develop03/Programs.cs
@@ -60,103 +60,47 @@ class Program
 
 
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using CsvHelper;
-using CsvHelper.Configuration;
-
-public class DataClass
-{
-    public int Field1 { get; set; }
-    public string Book { get; set; }
-    public int Chapter { get; set; }
-    public int Verse { get; set; }
-    public int Field5 { get; set; }
-    public string Text { get; set; }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
-        var records = ReadDataFromCSV("D:/BYU 3 SEMESTER/cse210-project/prove/Develop03/bible.csv");
+        string filePath = "D:/BYU 3 SEMESTER/cse210-project/prove/Develop03/bible.csv";
 
-        Console.WriteLine("Choose an option:");
-        Console.WriteLine("1. Show specific data");
-        Console.WriteLine("2. Show random data");
-        Console.Write("Option: ");
-
-        if (int.TryParse(Console.ReadLine(), out int option))
+        if (File.Exists(filePath))
         {
-            switch (option)
+            try
             {
-                case 1:
-                    ShowSpecificData(records);
-                    break;
-                case 2:
-                    ShowRandomData(records);
-                    break;
-                default:
-                    Console.WriteLine("Invalid option.");
-                    break;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Invalid input.");
-        }
-    }
-
-    static List<DataClass> ReadDataFromCSV(string filePath)
-    {
-        using (var reader = new StreamReader(filePath))
-        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
-        {
-            return csv.GetRecords<DataClass>().ToList();
-        }
-    }
-
-    static void ShowSpecificData(List<DataClass> records)
-    {
-        Console.Write("Enter the name of the book (e.g., '1 Kings'): ");
-        string book = Console.ReadLine();
-        Console.Write("Enter the chapter number: ");
-        if (int.TryParse(Console.ReadLine(), out int chapter))
-        {
-            Console.Write("Enter the verse number: ");
-            if (int.TryParse(Console.ReadLine(), out int verse))
-            {
-                var specificData = records.FindAll(d => d.Book == book && d.Chapter == chapter && d.Verse == verse);
-                if (specificData.Count > 0)
+                using (StreamReader reader = new StreamReader(filePath))
                 {
-                    foreach (var data in specificData)
+                    while (!reader.EndOfStream)
                     {
-                        Console.WriteLine($"{data.Field1}, {data.Book}, {data.Chapter}, {data.Verse}, {data.Field5}, {data.Text}");
+                        string line = reader.ReadLine();
+                        string[] fields = line.Split(',');
+                        if (fields.Length >= 6)
+                        {
+                            int field1 = int.Parse(fields[0]);
+                            string book = fields[1];
+                            int chapter = int.Parse(fields[2]);
+                            int verse = int.Parse(fields[3]);
+                            int field5 = int.Parse(fields[4]);
+                            string text = fields[5];
+
+                            Console.WriteLine($"{field1}, {book}, {chapter}, {verse}, {field5}, {text}");
+                        }
                     }
                 }
-                else
-                {
-                    Console.WriteLine("No data found for the specified input.");
-                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Invalid verse input.");
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
         else
         {
-            Console.WriteLine("Invalid chapter input.");
+            Console.WriteLine("The file does not exist at the specified path.");
         }
-    }
-
-    static void ShowRandomData(List<DataClass> records)
-    {
-        var random = new Random();
-        int randomIndex = random.Next(records.Count);
-        var randomData = records[randomIndex];
-        Console.WriteLine($"{randomData.Field1}, {randomData.Book}, {randomData.Chapter}, {randomData.Verse}, {randomData.Field5}, {randomData.Text}");
     }
 }
 
