e1661e8de4ab3e51098b5f04d5050558ef6adb02
JPgitidaho <122384707+JPgitidaho@users.noreply.github.com>
2023-11-19 00:25:45 -0300

    Develop 05

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,4 +1,3 @@
-
 // Exceeding Requirements: The added functionality implements motivational notifications to remind users to log events or progress towards their goals.
 // A timer automatically starts every 24 hours, sending a reminder message to users.
 // The added functionality includes a menu option that allows users to return to the main menu.
@@ -9,48 +8,48 @@ static class NotificationManager
 
     public static void StartNotificationTimer()
     {
-        // Inicia el temporizador con un intervalo de 24 horas (86400000 milisegundos)
+        // Starts the timer with an interval of 24 hours (86400000 milliseconds)
         notificationTimer = new Timer(SendMotivationalNotification, null, 0, 86400000);
     }
 
     private static void SendMotivationalNotification(object state)
     {
-        // Puedes personalizar el mensaje de la notificación motivadora
+        // You can customize the motivational notification message
         Console.WriteLine(">>>>>>>>>>>>Remember to log events for your goals today!");
     }
 }
+
 static class Menu
 {
     // List to store the goals
     static List<Goal> Goals = new List<Goal>();
 
     // Main method to display the menu and handle user input
-public static void Main()
-{
-    // Inicia el temporizador de notificaciones al inicio del programa
-    NotificationManager.StartNotificationTimer();
-
-    // Display total points
-    Console.WriteLine($"**** Your Total Points: {Goal.Totalpoints} ****");
-    Console.WriteLine();
-
-    // Muestra el mensaje de recordatorio
-    Console.WriteLine(">>>>>>>>>>>>Remember to log events for your goals today!");
-    Console.WriteLine();
-
-    List<string> Options = new List<string> { "Create New Goal", "Record Event", "List Goals", "Save Goals", "Load Goals", "Quit" };
-
-    // Agrega la opción "Back" al final del menú
-    Options.Add("Back");
-
-    foreach (var option in Options.Select((value, index) => new { Index = index + 1, Value = value }))
+    public static void Main()
     {
-        Console.WriteLine($"{option.Index}. {option.Value}");
-    }
+        // Starts the notification timer at the beginning of the program
+        NotificationManager.StartNotificationTimer();
 
-    Console.Write("Select a choice from the menu: ");
-    string input = Console.ReadLine();
+        // Display total points
+        Console.WriteLine($"**** Your Total Points: {Goal.Totalpoints} ****");
+        Console.WriteLine();
 
+        // Displays the reminder message
+        Console.WriteLine(">>>>>>>>>>>>Remember to log events for your goals today!");
+        Console.WriteLine();
+
+        List<string> Options = new List<string> { "Create New Goal", "Record Event", "List Goals", "Save Goals", "Load Goals", "Quit" };
+
+        // Adds the "Back" option at the end of the menu
+        Options.Add("Back");
+
+        foreach (var option in Options.Select((value, index) => new { Index = index + 1, Value = value }))
+        {
+            Console.WriteLine($"{option.Index}. {option.Value}");
+        }
+
+        Console.Write("Select a choice from the menu: ");
+        string input = Console.ReadLine();
 
         // Handle user choice
         switch (input)
@@ -79,7 +78,7 @@ public static void Main()
             case "6":
                 Console.WriteLine("Best of luck in achieving your goals!");
                 break;
-            case "7": // Esta es la nueva opción "Back" para volver al inicio
+            case "7": // This is the new "Back" option to return to the beginning
                 Main();
                 break;
             default:
@@ -132,6 +131,7 @@ public static void Main()
                 currentGoals.Add(i);
             }
         }
+
         for (int i = 0; i < currentGoals.Count; i++)
         {
             int CG = currentGoals[i];
@@ -139,12 +139,16 @@ public static void Main()
             Console.Write($"\n {i + 1}. ");
             Goals[CG].DisplayGoal();
         }
+
         if (currentGoals.Count == 0)
         {
             Console.WriteLine("You have no goals at the moment");
         }
+
+        // Adjusted the option label to match the pattern
         Console.WriteLine($"{currentGoals.Count + 1}. Back");
         int input = GoalManager.ReadInt("Choose an option: ") - 1;
+
         if (input == currentGoals.Count)
         {
             Main();
